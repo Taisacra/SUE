8a44063711f168b0c6bbaf0ca3417b8e33a223d2
Tainá Sacramento <tainasacramento2002@gmail.com>
2024-10-16 21:22:55 -0300

    Atualizações react

--- client/sue/src/App.js
+++ client/sue/src/App.js
@@ -1,71 +1,16 @@
 function App() {
   return (
-    <nav className="sidebar">
-    <div className="App">
-      <img src="./img/logo_sue.jpeg" id="icone_sue" alt="Sue"></img>
-      <p className="sue_infos">
-        <span className="itemDescription">
-          SUE
-        </span>
-        <span className="itemDescription">
-          - Sistema Unificado Estudantil
-        </span>
-      </p>
+    <div className="container">
+      <div calssName="content">
+       <button type="button" onClick={}> Módulo Acadêmico </button>
+       <button type="button" onClick={}> Módulo de Gestão Administrativa </button>
+      </div>
+      <footer>
+        <p>&copy; 2024 SUE- SISTEMA UNIFICADO ESTUDANTIL. Todos os diteiros reservados. </p>
+      </footer>
     </div>
-    <ul className="sideItens">
-      <li className="sideItem">
-        <a href="#">
-        <span className="itemDescricao">
-          Usuario
-        </span>
-        </a>  
-      </li>
-      <li className="sideItem">
-      <a href="#">
-        <span className="itemDescricao">
-          Aluno
-        </span>
-        </a>
-      </li>
-      <li className="sideItem">
-      <a href="#">
-        <span className="itemDescricao">
-          Professor
-        </span>
-        </a>
-      </li>
-      <li className="sideItem">
-      <a href="#">
-        <span className="itemDescricao">
-          Coordenador
-        </span>
-        </a>
-      </li>
-      <li className="sideItem">
-      <a href="#">
-        <span className="itemDescricao">
-          Curso
-        </span>
-        </a>
-      </li>
-      <li className="sideItem">
-      <a href="#">
-        <span className="itemDescricao">
-          Turma
-        </span>
-        </a>
-      </li>
-      <li className="sideItem">
-        <a href="#">
-          <span className="itemDescricao">
-          Pagamento
-          </span>
-        </a>
-      </li>
-    </ul>
-   
-    </nav>
-  );
+    
+   );
 }
 
 export default App;
